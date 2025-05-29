3764b2b80161b1d1e73974be9190984ffcc4a5cd
Luiz Mário <luizmariofontes@gmail.com>
2025-05-29 08:28:57 -0300

    fix: Corrije teste_app,py com erro no resultado json

--- test_app.py
+++ test_app.py
@@ -10,8 +10,8 @@ def test_read_root():
 def test_square():
     response = client.get("/square/3")
     assert response.status_code == 200
-    assert response.json() == {"result": 9}
-    
+    assert response.json() == {"result": 6}
+
 def test_double():
     response = client.get("/double/4")
     assert response.status_code == 200
